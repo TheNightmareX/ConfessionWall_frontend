f23fb93db681626f6985aeba5a7a4ae37f3cf406
TheNightmareX <TheNightmareX@foxmail.com>
2020-10-31 08:20:31 +0800

    Fix production deployment bugs.

--- src/apis.js
+++ src/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV == 'production' ? '' : 'http://localhost:8000/api/',
+    baseURL: process.env.NODE_ENV == 'production' ? '/api/' : 'http://localhost:8000/api/',
     xsrfCookieName: 'csrftokan',
     xsrfHeaderName: 'X-CSRFToken'
 })
