1ba5d8f149854336099eec31cb5ecc5f7e378391
TheNightmareX <TheNightmareX@foxmail.com>
2020-11-15 18:59:33 +0800

    Use `localStorage` as local session storage. Update README.md

--- src/storage/index.js
+++ src/storage/index.js
@@ -1,32 +1,71 @@
+import dayjs from "dayjs";
 import Vue from "vue";
-import { session } from "./serializers";
+import { local } from "./serializers";
+
+/**@typedef {import("dayjs").Dayjs} Dayjs */
+
+const VERSION = '1.5.2'
+
+const storage = new Vue({
+  data: {
+    /** @type {Object<number, boolean>} */
+    liked: undefined,
+    /** @type {Object<number, boolean>} */
+    commented: undefined,
+    /**@type {number} */
+    confessionCount: undefined,
+    /**@type {boolean} */
+    authed: undefined,
+    /**@type {Dayjs} */
+    expireDate: undefined,
+    /**@type {string} */
+    VERSION: undefined,
+  },
+
+  methods: {
+    reset() {
+      this.liked = {};
+      this.commented = {};
+      this.confessionCount = 0;
+      this.authed = false;
+      this.expireDate = dayjs().add(2, "week");
+      this.VERSION = VERSION;
+    },
+    load() {
+      for (const k in this.$data) {
+        if (k in local) {
+          this[k] = local[k];
+        }
+      }
+      this.expireDate = dayjs(this.expireDate);
+    },
+    save() {
+      this.expireDate = this.expireDate.format();
+      Object.assign(local, this.$data);
+    },
+  },
+
+  created() {
+    this.reset();
+    this.load();
+    if (this.expireDate.isBefore(dayjs()) || this.VERSION != VERSION) {
+      this.reset();
+    }
+
+    console.debug(this.$data);
+  },
+});
 
 let save = true;
 
 window.reset = () => {
   save = false;
-  sessionStorage.clear();
   localStorage.clear();
   location.reload();
 };
 
-export default new Vue({
-  data: {
-    /** @type {Object<number, boolean>} */
-    liked: session.liked || {},
-    /** @type {Object<number, boolean>} */
-    commented: session.commented || {},
-    /**@type {number} */
-    confessionCount: session.confessionCount || 0,
-    /**@type {boolean} */
-    authed: session.authed || false,
-  },
-
-  created() {
-    console.debug(this.$data);
-    window.addEventListener("unload", () => {
-      if (!save) return;
-      Object.assign(session, this.$data);
-    });
-  },
+window.addEventListener("unload", () => {
+  if (save) storage.save();
 });
+
+export default storage;
